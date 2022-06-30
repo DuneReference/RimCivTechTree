2d6e3804d38faaa76282d42c86e2c9cbada02e32
DuneReference <hosstay@hotmail.com>
2022-06-30 12:12:23 -0500

    initial commit

---
+++ 1.3/Source/RimCiv/DuneRef_RimCivTechTree_DefOf.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using Verse;
+
+namespace DuneRef_RimCivTechTree
+{
+    [DefOf]
+    public class DuneRef_RimCivTechTree_DefOf
+    {
+        public static ResearchProjectDef DuneRef_Currency;
+    }
+}
