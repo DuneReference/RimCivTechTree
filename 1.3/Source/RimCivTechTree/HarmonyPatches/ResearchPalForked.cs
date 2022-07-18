081cb8d58bfd88ab0035e18bbbf6f491f028e18c
DuneReference <hosstay@hotmail.com>
2022-07-17 20:37:54 -0500

    Refactored ResearchPalForked to use new utility file for getting unlocked defs. dubs from last commit uses this too

--- 1.3/Source/RimCivTechTree/HarmonyPatches/ResearchPalForked.cs
+++ 1.3/Source/RimCivTechTree/HarmonyPatches/ResearchPalForked.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using RimWorld;
 using System.Collections.Generic;
 using System.Linq;
+using ResearchPal;
 
 namespace DuneRef_RimCivTechTree
 {
@@ -16,7 +17,13 @@ namespace DuneRef_RimCivTechTree
 
         public static void Patches()
         {
-            // Hide nodes to be hidden
+            // Finish linked projects
+            Harm.Patch(AccessTools.Method(typeof(ResearchPal.HarmonyPatches_Queue.DoCompletionDialog), "Postfix"), prefix: new HarmonyMethod(CommonPatches.patchType, nameof(CommonPatches.FinishProjectOmniFix)));
+
+            // Show icons of linked projects for my projects 
+            Harm.Patch(AccessTools.Method(typeof(ResearchPal.ResearchProjectDef_Extensions), nameof(ResearchPal.ResearchProjectDef_Extensions.GetUnlockDefs)), prefix: new HarmonyMethod(patchType, nameof(GetUnlockDefsPrefix)));
+
+            // Hide projects that I designate for hiding.
             MethodInfo populateNodesInnerMethod = AccessTools.FindIncludingInnerTypes(
                 typeof(ResearchPal.Tree),
                 (type) => AccessTools.FirstMethod(
@@ -25,109 +32,28 @@ namespace DuneRef_RimCivTechTree
                 )
             );
             Harm.Patch(populateNodesInnerMethod, postfix: new HarmonyMethod(patchType, nameof(ResearchPalTreePopulateNodesHiddenPostfix)));
-
-            // Unlock techs this higher level tech is associated with
-            Harm.Patch(AccessTools.Method(typeof(ResearchPal.HarmonyPatches_Queue.DoCompletionDialog), "Postfix"), prefix: new HarmonyMethod(CommonPatches.patchType, nameof(CommonPatches.FinishProjectOmniFix)));
-
-            // Make icons in nodes show up on higher level techs based on the lower nodes they link to.
-            Harm.Patch(AccessTools.Method(typeof(ResearchPal.ResearchProjectDef_Extensions), nameof(ResearchPal.ResearchProjectDef_Extensions.GetPlantsUnlocked)), prefix: new HarmonyMethod(patchType, nameof(GetPlantsUnlockedPrefix)));
-            Harm.Patch(AccessTools.Method(typeof(ResearchPal.ResearchProjectDef_Extensions), nameof(ResearchPal.ResearchProjectDef_Extensions.GetRecipesUnlocked)), prefix: new HarmonyMethod(patchType, nameof(GetRecipesUnlockedPrefix)));
-            Harm.Patch(AccessTools.Method(typeof(ResearchPal.ResearchProjectDef_Extensions), nameof(ResearchPal.ResearchProjectDef_Extensions.GetTerrainUnlocked)), prefix: new HarmonyMethod(patchType, nameof(GetTerrainUnlockedPrefix)));
-            Harm.Patch(AccessTools.Method(typeof(ResearchPal.ResearchProjectDef_Extensions), nameof(ResearchPal.ResearchProjectDef_Extensions.GetThingsUnlocked)), prefix: new HarmonyMethod(patchType, nameof(GetThingsUnlockedPrefix)));
         }
 
-        // Harmony Functions
+        public static bool GetUnlockDefsPrefix(ref List<Def> __result, ResearchProjectDef research, ref Dictionary<Def, List<Def>> ____unlocksCache)
+        {
+            if (____unlocksCache.ContainsKey(research))
+            {
+                __result = ____unlocksCache[research];
+            } else
+            {
+                __result = Utility.GetUnlocksForResearch(research)
+                    .Where(x => x.IconTexture() != null)
+                    .ToList();
+
+                ____unlocksCache.Add(research, __result);
+            }
+                
+            return false;
+        }
+
         public static bool ResearchPalTreePopulateNodesHiddenPostfix(bool __result, ResearchProjectDef p)
         {
             return __result || (p.tab != null && p.tab == RimCivTechTree_DefOf.DuneRef_Hidden);
-        }
-
-        public static bool GetPlantsUnlockedPrefix(ref IEnumerable<ThingDef> __result, ResearchProjectDef research)
-        {
-            __result = DefDatabase<ThingDef>.AllDefs.Where((td) =>
-            {
-                bool found = td.plant?.sowResearchPrerequisites?.Contains(research) ?? false;
-
-                if (!found && research.GetModExtension<ResearchUnlocks>() != null)
-                {
-                    foreach (ResearchProjectDef unlock in research.GetModExtension<ResearchUnlocks>().researchUnlocks)
-                    {
-                        found = td.plant?.sowResearchPrerequisites?.Contains(unlock) ?? false;
-                        if (found) break;
-                    }
-                }
-
-                return found;
-            });
-
-            return false;
-        }
-
-        public static bool GetRecipesUnlockedPrefix(ref IEnumerable<RecipeDef> __result, ResearchProjectDef research)
-        {
-            __result = DefDatabase<RecipeDef>.AllDefs.Where((rd) =>
-            {
-                bool found = rd.researchPrerequisite == research ||
-                             (rd.researchPrerequisites != null &&
-                              rd.researchPrerequisites.Contains(research));
-
-                if (!found && research.GetModExtension<ResearchUnlocks>() != null)
-                {
-                    foreach (ResearchProjectDef unlock in research.GetModExtension<ResearchUnlocks>().researchUnlocks)
-                    {
-                        found = rd.researchPrerequisite == unlock ||
-                               (rd.researchPrerequisites != null &&
-                                rd.researchPrerequisites.Contains(unlock));
-                        if (found) break;
-                    }
-                }
-
-                return found;
-            });
-
-            return false;
-        }
-
-        public static bool GetTerrainUnlockedPrefix(ref IEnumerable<TerrainDef> __result, ResearchProjectDef research)
-        {
-            __result = DefDatabase<TerrainDef>.AllDefs.Where((td) =>
-            {
-                bool found = td.researchPrerequisites?.Contains(research) ?? false;
-
-                if (!found && research.GetModExtension<ResearchUnlocks>() != null)
-                {
-                    foreach (ResearchProjectDef unlock in research.GetModExtension<ResearchUnlocks>().researchUnlocks)
-                    {
-                        found = td.researchPrerequisites?.Contains(unlock) ?? false;
-                        if (found) break;
-                    }
-                }
-
-                return found;
-            });
-
-            return false;
-        }
-
-        public static bool GetThingsUnlockedPrefix(ref IEnumerable<ThingDef> __result, ResearchProjectDef research)
-        {
-            __result = DefDatabase<ThingDef>.AllDefs.Where((td) =>
-            {
-                bool found = td.researchPrerequisites?.Contains(research) ?? false;
-
-                if (!found && research.GetModExtension<ResearchUnlocks>() != null)
-                {
-                    foreach (ResearchProjectDef unlock in research.GetModExtension<ResearchUnlocks>().researchUnlocks)
-                    {
-                        found = td.researchPrerequisites?.Contains(unlock) ?? false;
-                        if (found) break;
-                    }
-                }
-
-                return found;
-            });
-
-            return false;
         }
     }
 }
